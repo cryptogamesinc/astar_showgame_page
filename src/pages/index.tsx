a0f176458149e71966554d8d4c1da808e7a8e940
ytakahashi2020 <y.takahashi2020@gmail.com>
2023-05-25 16:11:09 +0900

    mainContractAddressの修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,7 +59,7 @@ const override = css`
   `;
 
 
-const mainContractAddress = "Wf3eoq4wxTtjbUSAZ3gAmE3y8WbmVN6ruxuoTzBjyQkyENW"
+const mainContractAddress = "XHf8TYxVERU4MZMPQj8fTVTZi9bgTzectGEVydEDzhAVvDF"
 const psp22ContractAddress = "WG7GLbCQLnuCyiURRaFsCsmg2E87mwbjoNvT675rxs5tgXe"
 const psp37ContractAddress = "VwRKvqjLhK4NBBwmq3QkLVcdycfqghwe8iMcs95PFQj3A3x"
 
